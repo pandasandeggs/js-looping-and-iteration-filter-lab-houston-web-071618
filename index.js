7c313ee3260eb1bbad63525f914241ef1e5d3c2b
pandasandeggs <theteatreehouse@gmail.com>
2018-09-03 14:43:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(array,name){
+  return array.filter(function(driver){
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(array,name){
+  let nameLength = name.length;
+  return array.filter(function(driver){
+    return driver.slice(0,nameLength) === name;
+  });
+}
+
+function matchName(array,name){
+  return array.filter(function(driver){
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
